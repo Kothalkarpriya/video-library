8dce67cb47765f829ed8af48005cec9a8b70b887
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-18 23:50:55 +0530

    feat- pages and router added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,6 +7,12 @@ export default function Navbar() {
       <Link to="/">Home || </Link>
       <Link to="/Playlist">Playlist || </Link>
       <Link to="/VideoListing">VideoListing </Link>
+      <Link to="/Login">Login || </Link>
+      <Link to="/SignUp">SignUp || </Link>
+      <Link to="/Logout">Logout|| </Link>
+      <Link to="/History">History || </Link>
+      <Link to="/Favourites">Favourites || </Link>
+      <Link to="/WatchLater">WatchLater </Link>
     </nav>
   );
 }
