703c0003c2e2ae899498d3ffc1f5a0147f900bd6
Priya Kothalkar <priyakothalkar2973@gmail.com>
2022-04-21 21:43:02 +0530

    feat-dataBase for videos added

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -6,16 +6,110 @@
 export const videos = [
   {
     _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    title: "Computation Thinking & Scratch-Intro to Computer Science",
+    creator: "David J. Malan -Harvard University",
     description:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    videoImage:
+      "https://i.ytimg.com/vi/F0WoVEr0-44/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCyb0oW_6V-oKPK64XalvhC5-OSiA",
+    views: "10",
+    videoCreated: "10",
+    videoUrl: "https://youtu.be/F0WoVEr0-44",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "Wo5dMEP_BbI",
+    title: "C Programming Language -Intro to Computer Science",
+    creator: "David J. Malan -Harvard University",
     description:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    videoImage:
+      "https://i.ytimg.com/vi/ix5jPkxsr7M/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCQMuwYn6FwORg0pteLpe1KJBHfQQ",
+    views: "10",
+    videoCreated: "10",
+    videoUrl: "https://youtu.be/ix5jPkxsr7M",
+  },
+  {
+    _id: "Wo5dMEP_BbI",
+    title: "Arrays and Sorting Algorithms -Intro to Computer Science",
+    creator: "David J. Malan -Harvard University",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    videoImage:
+      "https://i.ytimg.com/vi/ba7e4mksR5I/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLByEsaXW3--CTcFCduyJhvk5Mb_Vw",
+    views: "10",
+    videoCreated: "10",
+    videoUrl: "https://youtu.be/ba7e4mksR5I",
+  },
+  {
+    _id: "Wo5dMEP_BbI",
+    title: "Memory -Intro to Computer Science",
+    creator: "David J. Malan -Harvard University",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    videoImage:
+      "https://i.ytimg.com/vi/pejxLkT-wek/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCAzU8zcMtztbQXOkj06m4mAwgp0g",
+    views: "10",
+    videoCreated: "10",
+    videoUrl: "https://youtu.be/pejxLkT-wek",
+  },
+  {
+    _id: "Wo5dMEP_BbI",
+    title: "Data Structure-Intro to Computer Science",
+    creator: "David J. Malan -Harvard University",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    videoImage:
+      "https://i.ytimg.com/vi/uTZFGXv798o/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAoswg-SX-ImbGlibv3jXFjJuNvSg",
+    views: "10",
+    videoCreated: "10",
+    videoUrl: "https://youtu.be/uTZFGXv798o4",
+  },
+  {
+    _id: "Wo5dMEP_BbI",
+    title: "HTTP, HTML & CSS-Intro to Computer Science",
+    creator: "David J. Malan -Harvard University",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    videoImage:
+      "https://i.ytimg.com/vi/mudOTSUumXg/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBKis5nrEwdYQQrc26p8lII2EVajA",
+    views: "10",
+    videoCreated: "10",
+    videoUrl: "https://youtu.be/mudOTSUumXg",
+  },
+  {
+    _id: "Wo5dMEP_BbI",
+    title: "Python-Intro to Computer Science",
+    creator: "David J. Malan -Harvard University",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    videoImage:
+      "https://i.ytimg.com/vi/hnDU1G9hWqU/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBKLEZd4hfwiLxCHaI6ExcKArJcTw",
+    views: "10",
+    videoCreated: "10",
+    videoUrl: "https://youtu.be/hnDU1G9hWqU",
+  },
+  {
+    _id: "Wo5dMEP_BbI",
+    title: "Web Programming with Flask-Intro to Computer Science",
+    creator: "David J. Malan -Harvard University",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    videoImage:
+      "https://i.ytimg.com/vi/zdgYw-3tzfI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAb5DPrrqyOTOYjeOW42S1LN8B40Q",
+    views: "10",
+    videoCreated: "10",
+    videoUrl: "https://youtu.be/zdgYw-3tzfI",
+  },
+  {
+    _id: "Wo5dMEP_BbI",
+    title: "SQL-Intro to Computer Science",
+    creator: "David J. Malan -Harvard University",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    videoImage:
+      "https://i.ytimg.com/vi/TplT4qjz1RQ/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCOLkF5IdycMtoDdyP9fZ0zo6LNjg",
+    views: "10",
+    videoCreated: "10",
+    videoUrl: "https://youtu.be/TplT4qjz1RQ",
   },
 ];
